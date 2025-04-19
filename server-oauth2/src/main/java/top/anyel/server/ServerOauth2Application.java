eb434d475762179bd2e2f3e92a57b6eca18777cb
anyel-ec <paulangel826@gmail.com>
2025-04-18 19:39:47 -0500

    chore: start project the server oauth2

---
+++ server-oauth2/src/main/java/top/anyel/server/ServerOauth2Application.java
@@ -0,0 +1,13 @@
+package top.anyel.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerOauth2Application.class, args);
+    }
+
+}
