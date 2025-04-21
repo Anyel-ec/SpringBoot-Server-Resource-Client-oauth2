696d6a7a687ea24e7df49df5615cce725d31664d
anyel-ec <paulangel826@gmail.com>
2025-04-20 19:29:09 -0500

    chore: start project in spring client the oauth2

---
+++ spring-client-oauth2/src/main/java/top/anyel/client/SpringClientOauth2Application.java
@@ -0,0 +1,13 @@
+package top.anyel.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientOauth2Application.class, args);
+    }
+
+}
---
+++ spring-client-oauth2/src/test/java/top/anyel/client/SpringClientOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
